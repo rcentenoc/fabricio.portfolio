0fd1e615aef61a90a35dce2962b29ed6d884e06f
Fabricio Centeno <rcentenoc@ulasalle.edu.pe>
2025-09-01 00:55:32 -0500

    Refactor routing to use BrowserRouter for improved compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import './index.css'
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx'
 import Archive from './sections/Archive.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <Routes>
       <Route path="/projectsArchive" element={<Archive />} />
       <Route path="/" element={<App />} />
     </Routes>
-  </HashRouter>
+  </BrowserRouter>
 )
